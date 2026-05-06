3a4c85aeee7e8ff55282442d9e661f413348294e
MateusDallaz <mdallaz667@gmail.com>
2026-05-06 20:02:14 -0300

    Funções

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-#importa a biblioteca para conectar conectar o banco de dados 
 import mysql.connector
 
+def conectar():
+    conexao = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='oficina'
+    )
+    print('Conectado')
+    return conexao
 
-#Executa uma fuinção da lib 
-conexao = mysql.connector.connect(
-    #partametros 
-    host = 'localhost',
-    user = 'root',
-    password = '',
-    database = 'oficina'
-)
+def inserir_servico(conexao):
+    mecanico = input('Digite o mecanico responsavel: ')
+    descricao = input('Digite a descrição do serviço realizado: ')
+    cursor = conexao.cursor()
+    sql = "INSERT INTO servico(mecanico_encarregado, descricao_servico) VALUES(%s, %s)"
+    values = (mecanico, descricao)
+    cursor.execute(sql, values)
+    conexao.commit()
+    print(f'Serviço inserido: {mecanico} - {descricao}')
 
-print('conectado')
+def listar_servicos(conexao):
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM servico")
+    resultado = cursor.fetchall()
+    for i in resultado:
+        print(i)
 
-#função cursor() da lib 
-cursor = conexao.cursor()
+def fechar_conexao(conexao):
+    conexao.close()
+    print('Conexão encerrada')
 
-#comandos de inserção de parametros
-sql = "INSERT INTO servico(mecanico_encarregado, descricao_servico) VALUES(%s, %s)"
-values = ("João", "Troca de oleo")
-
-cursor.execute(sql, values)
-conexao.commit()
-
-cursor.execute("SELECT * FROM servico")
-
-resultado = cursor.fetchall()
-
-for i in resultado:
-    print(i)
+# Uso
+conexao = conectar()
+inserir_servico(conexao)
+listar_servicos(conexao)
+fechar_conexao(conexao)
